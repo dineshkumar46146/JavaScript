e8c95e3d1f2be123040fdde8d052916a7d3d62bf
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-08-04 20:33:16 +0530

    bottom to top scroll button using JS

--- Website with JavaScript/scripts/JavaScript.js
+++ Website with JavaScript/scripts/JavaScript.js
@@ -19,4 +19,17 @@ for (i = 0; i<acc.length; i++) {
 		}
 	});
 }
+// bottom to top scroll
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
