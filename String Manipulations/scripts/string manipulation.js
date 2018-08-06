65605af07c8225e39ee129952f542abd9110be82
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-08-06 19:11:30 +0530

    strings

---
+++ String Manipulations/scripts/string manipulation.js
@@ -0,0 +1,25 @@
+// string using console.log
+var name = 'Dhinesh kumar';
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(8, 14));
+console.log(name.substr(8, 1));
+console.log(name.length);
+console.log(name.replace('a', '@'));
+var nameBits = name.split('h');
+console.log(nameBits);
+console.log(nameBits.length);
+console.log(nameBits[2]);
+// adding two strings
+var string1 = "hello";
+var string2 = "javascript";
+var result = string1 + " " + string2;
+var result1 = string1.concat(" ","hello", "world");
+console.log(result,result1);
+var string3 = "hello";
+var string4 = "string";
+var result2 = string3.trim() + string4.trim();
+console.log(result2);
+
+
+
